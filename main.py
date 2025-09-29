43747d824d012155248ab4ad01deb655e4c7315b
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2025-09-29 23:53:18 +0100

    Main

---
+++ main.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self, title, author, header, body, footer):
+        self.title = title
+        self.author = author
+        self.header = header
+        self.body = body
+        self.footer = footer
