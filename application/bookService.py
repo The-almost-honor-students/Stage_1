a78b43af7bb43d566d8d2454e534ca0204be10d6
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2025-10-04 21:16:33 +0100

    Changed path directory

--- application/bookService.py
+++ application/bookService.py
@@ -2,40 +2,43 @@ import requests
 from datetime import datetime
 from pathlib import Path
 import shutil
+import os
 
-def download_book(book_id:int, output_path:str):
-        START_MARKER = "*** START OF THE PROJECT GUTENBERG EBOOK"
-        END_MARKER = "*** END OF THE PROJECT GUTENBERG EBOOK"
+BASE_PATH = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-        output_path = Path(output_path)
+def download_book(book_id: int, output_path: str):
+    START_MARKER = "*** START OF THE PROJECT GUTENBERG EBOOK"
+    END_MARKER = "*** END OF THE PROJECT GUTENBERG EBOOK"
 
-        output_path.mkdir(parents=True, exist_ok=True)
-        url = f"https://www.gutenberg.org/cache/epub/{book_id}/pg{book_id}.txt"
-        response = requests.get(url)
-        response.raise_for_status()
-        text = response.text
-        if START_MARKER not in text or END_MARKER not in text:
-            return False
-        header, body_and_footer = text.split(START_MARKER, 1)
-        body, footer = body_and_footer.split(END_MARKER, 1)
+    output_path = BASE_PATH / output_path
+    output_path.mkdir(parents=True, exist_ok=True)
 
-        body_path = output_path / f"{book_id}_body.txt"
-        header_path = output_path / f"{book_id}_header.txt"
-        with open(body_path, "w", encoding="utf-8") as f:
-            f.write(body.strip())
-        with open(header_path, "w", encoding="utf-8") as f:
-            f.write(header.strip())
-        return True
+    url = f"https://www.gutenberg.org/cache/epub/{book_id}/pg{book_id}.txt"
+    response = requests.get(url)
+    response.raise_for_status()
+    text = response.text
+    if START_MARKER not in text or END_MARKER not in text:
+        return False
+    header, body_and_footer = text.split(START_MARKER, 1)
+    body, footer = body_and_footer.split(END_MARKER, 1)
+
+    body_path = output_path / f"{book_id}_body.txt"
+    header_path = output_path / f"{book_id}_header.txt"
+    with open(body_path, "w", encoding="utf-8") as f:
+        f.write(body.strip())
+    with open(header_path, "w", encoding="utf-8") as f:
+        f.write(header.strip())
+    return True
 
 
 def create_datalake(book_id: int):
     date = datetime.now().strftime("%Y%m%d")
     hour = datetime.now().strftime("%H")
 
-    datalake_dir = Path("datalake") / date / hour
+    datalake_dir = BASE_PATH / "datalake" / date / hour
     datalake_dir.mkdir(parents=True, exist_ok=True)
 
-    downloads_dir = Path("staging/downloads")
+    downloads_dir = BASE_PATH / "staging/downloads"
     body_src = downloads_dir / f"{book_id}_body.txt"
     header_src = downloads_dir / f"{book_id}_header.txt"
 
@@ -51,8 +54,3 @@ def create_datalake(book_id: int):
 
     print(f"Archivos movidos a {datalake_dir.resolve()}")
     return True
-
-
-if __name__ == "__main__":
-    create_datalake(1342)
-
